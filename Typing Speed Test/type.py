1a78e0474c35c86f91ccd1fea1ba7f029be13efb
unknown <iltfhuss@gmail.com>
2024-07-30 23:04:26 +0500

    added typing speed test file

--- Typing Speed Test/type.py
+++ Typing Speed Test/type.py
@@ -16,15 +16,32 @@ def mistake(partest, usertest):
     return error
     
 
+def speed_time(time_s, time_e, userinput):
+    time_delay = time_e - time_s
+    time_R = round(time_delay, 2)
+    speed = len(userinput) / time_R
+    return round(speed)
+
+
+
 test = ["A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea.",
         "My name is Altaf Hussain" , "Welcome to our website"]
 
 
-rest1 = r.choice(test)
+test1 = r.choice(test)
 print("     ***** typing speed *****")
+
 
 print(test1)
 print()
 print()
 
+
+time_1 = time()
 testinput = input("Enter: ")
+time_2 = time()
+
+
+print('Speed: ', speed_time(time_1, time_2, testinput), "w/sec")
+print("Error: ", mistake(test1, testinput))
+
