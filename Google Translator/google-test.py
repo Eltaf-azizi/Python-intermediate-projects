ef408751679826756b85f9d8a3d0d4c157ad70ce
unknown <iltfhuss@gmail.com>
2024-08-02 20:38:04 +0500

    added python file

--- Google Translator/google-test.py
+++ Google Translator/google-test.py
@@ -11,15 +11,27 @@ root.config(bg='Red')
 frame = Frame(root).pick(side=BOTTOM)
 
 
-lab_txt = Label(root, text= "Sourse Text", font=("Time New Roman", 20, "Bold"), fg="Black")
+lab_txt = Label(root, text= "Sourse Text", font=("Time New Roman", 20, "Bold"), fg="Black"bg="Red")
 
 lab_txt.place(x=100, y=100, height=20, width=300)
 
 
 
-Sortxt = Text(frame, font=("Time New Roman", 20, "bold"), wrap=WORD)
+Sor_txt = Text(frame, font=("Time New Roman", 20, "bold"), wrap=WORD)
 
-Sortxt.place(x=10, y=130, height=200, width=480)
+Sor_txt.place(x=10, y=130, height=140, width=480)
 
+list_text = [1, ,2 3, 4]
+
+comb_sor = ttk.combobox(frame, value = list_text)
+
+comb_sor.place(x=10, y=300, height=40, width=100)
+
+comb_sor.set("english")
+
+
+button_change = Button(frame, text="Translate", relief=RAISED)
+
+button_change.place(x=120, y=300, height=40, width=100)
 
 root.mainloop()
