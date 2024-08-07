8c6e94f90ceaf6989e79eb8b5a309fc58dc62318
unknown <iltfhuss@gmail.com>
2024-08-07 20:37:50 +0500

    added code

--- Digital Clock/digital.py
+++ Digital Clock/digital.py
@@ -1,3 +1,16 @@
 from tkinter import *
 
+clock = Tk()
 
+clock.title('     **** Digital Clock ****')
+clock.geometry('1000x500')
+clock.config(bg='yellow')
+
+lab_hr = Label(clock, text="00", font=('Time New Roman', 70,"bold"),
+               bg='red',fg='white')
+
+lab_hr.place(x=40, y=40,height=100, width=140)
+
+
+
+clock.mainloop()
