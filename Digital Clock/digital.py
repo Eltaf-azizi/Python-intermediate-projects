edac3930f75f7111655ad0b8fa32ddea07b276ba
unknown <iltfhuss@gmail.com>
2024-08-09 23:50:57 +0500

    added code

--- Digital Clock/digital.py
+++ Digital Clock/digital.py
@@ -3,7 +3,17 @@ import datetime
 
 
 
-
+def date_time() :
+    time = datetime.datetime.now()
+    hr = time.strftime('%I')
+    mi = time.strftime('%M')
+    sec = time.strftime('%S')
+    am = time.strftime('%p')
+    lab_hr.config(text=hr)
+    lab_min.config(text=mi)
+    lab_sec.config(text=sec)
+    lab_am.config(text=am)
+    lab_hr.after(200,date_time)
 
 
 
@@ -15,6 +25,8 @@ clock.geometry('1000x500')
 clock.config(bg='yellow')
 
 
+# ****** Time
+
 lab_hr = Label(clock, text="00", font=('Time New Roman', 70,"bold"),
                bg='red',fg='white')
 lab_hr.place(x=120, y=49,height=100, width=100)
@@ -23,7 +35,7 @@ lab_hr.place(x=120, y=49,height=100, width=100)
 
 lab_hr_txt = Label(clock, text="Hour", font=('Time New Roman', 30,"bold"),
                bg='red',fg='white')
-lab_hr_txt.place(x=120, y=190,height=4000), width=1
+lab_hr_txt.place(x=120, y=190,height=40, width=100)
 
 
 
@@ -35,7 +47,7 @@ lab_min.place(x=340, y=49,height=100, width=100)
 
 lab_min_txt = Label(clock, text="Min.", font=('Time New Roman', 30,"bold"),
                bg='red',fg='white')
-lab_min_txt.place(x=340, y=190,height=4000), width=1
+lab_min_txt.place(x=340, y=190,height=40, width=100)
 
 
 
@@ -45,23 +57,35 @@ lab_sec.place(x=570, y=49,height=100, width=100)
 
 
 
-lab_sec_txt = Label(clock, text="Sec.", font=('Time New Roman', 30,"bold"),
+lab_sec_txt = Label(clock, text="Min.", font=('Time New Roman', 30,"bold"),
                bg='red',fg='white')
-lab_sec_txt.place(x=570, y=190,height=4000), width=1
+lab_sec_txt.place(x=340, y=190,height=40, width=100)
 
 
-
-lab_am = Label(clock, text="00", font=('Time New Roman', 70,"bold"),
+lab_am = Label(clock, text="00", font=('Time New Roman', 47,"bold"),
                bg='red',fg='white')
 lab_am.place(x=780, y=49,height=100, width=100)
 
 
 
-lab_am_txt = Label(clock, text="AM/PM", font=('Time New Roman', 20,"bold"),
+lab_am_txt = Label(clock, text="Sec.", font=('Time New Roman', 30,"bold"),
                bg='red',fg='white')
-lab_am_txt.place(x=570, y=190,height=4000), width=1
+lab_am_txt.place(x=570, y=190,height=40,width=100)
 
 
 
+# ******** Date
+lab_date = Label(clock, text="00", font=('Time New Roman', 70,"bold"),
+               bg='red',fg='white')
+lab_date.place(x=120, y=49,height=100, width=100)
+
+
+
+lab_date_txt = Label(clock, text="Date", font=('Time New Roman', 30,"bold"),
+               bg='red',fg='white')
+lab_date_txt.place(x=120, y=190,height=40, width=100)
+
+
+date_time()
 
 clock.mainloop()
