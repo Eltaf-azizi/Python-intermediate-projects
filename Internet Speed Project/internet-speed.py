78c8b8e365585fb5a8ac7c7049aa2fb432687b19
unknown <iltfhuss@gmail.com>
2024-08-14 00:08:02 +0500

    added code

--- Internet Speed Project/internet-speed.py
+++ Internet Speed Project/internet-speed.py
@@ -11,20 +11,24 @@ lab = Label(sp, text = "Internet Speed Tester", font=("Time new Roman", 30, "bol
 lab.place(x=100, y=40, height=49, width=480)
 
 
-lab = Label(sp, text = "Downloading Speed", font=("Time new Roman", 30, "bold"),bg="Blue",fg="White")
+lab = Label(sp, text = "Downloading Speed", font=("Time new Roman", 30, "bold"))
 lab.place(x=100, y=130, height=49, width= 480)
 
 
-lab = Label(sp, text = "00", font=("Time new Roman", 30, "bold"),bg="Blue",fg="White")
+lab = Label(sp, text = "00", font=("Time new Roman", 30, "bold"))
 lab.place(x=100, y=200, height=49, width=480)
 
 
-lab = Label(sp, text = "Upload Speed", font=("Time new Roman", 30, "bold"),bg="Blue",fg="White")
+lab = Label(sp, text = "Upload Speed", font=("Time new Roman", 30, "bold"))
 lab.place(x=100, y=290, height=49, width=480)
 
 
-lab = Label(sp, text = "00", font=("Time new Roman", 30, "bold"),bg="Blue",fg="White")
+lab = Label(sp, text = "00", font=("Time new Roman", 30, "bold"))
 lab.place(x=100, y=370, height=49, width=480)
+
+
+button = Button(sp, text="Check Speed", font=("Time New Roman", 30, "bold"),relief=RAISED)
+button.place(x=100, y= 370, height=49, width=480)
 
 
 
