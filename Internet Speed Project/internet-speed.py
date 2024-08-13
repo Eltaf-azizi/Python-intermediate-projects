a8c35944e6b7a8cbacdfccb6b6d57d5adf8c02ee
unknown <iltfhuss@gmail.com>
2024-08-13 23:27:53 +0500

    added code

--- Internet Speed Project/internet-speed.py
+++ Internet Speed Project/internet-speed.py
@@ -7,8 +7,27 @@ sp.title(" Internet Speed Tester")
 sp.geometry("700x700")
 sp.config(bg="Blue")
 
-lab = Label(sp, text = "Internet Speed Tester", font=("Time new Roman", 30, "bold"),bg="Blue",fg="Red")
-lab.place(x=100, y=40)
+lab = Label(sp, text = "Internet Speed Tester", font=("Time new Roman", 30, "bold"),bg="Blue",fg="White")
+lab.place(x=100, y=40, height=49, width=480)
+
+
+lab = Label(sp, text = "Downloading Speed", font=("Time new Roman", 30, "bold"),bg="Blue",fg="White")
+lab.place(x=100, y=130, height=49, width= 480)
+
+
+lab = Label(sp, text = "00", font=("Time new Roman", 30, "bold"),bg="Blue",fg="White")
+lab.place(x=100, y=200, height=49, width=480)
+
+
+lab = Label(sp, text = "Upload Speed", font=("Time new Roman", 30, "bold"),bg="Blue",fg="White")
+lab.place(x=100, y=290, height=49, width=480)
+
+
+lab = Label(sp, text = "00", font=("Time new Roman", 30, "bold"),bg="Blue",fg="White")
+lab.place(x=100, y=370, height=49, width=480)
+
+
+
 
 
 sp.mainloop()
