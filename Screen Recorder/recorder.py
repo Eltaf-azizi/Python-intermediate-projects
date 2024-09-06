fb88fd7883e7c20164336c9451489214bfadbd0a
unknown <iltfhuss@gmail.com>
2024-09-06 21:18:13 +0500

    added code

--- Screen Recorder/recorder.py
+++ Screen Recorder/recorder.py
@@ -6,16 +6,23 @@ import time
 
 width = getSystemMetrics(0)
 height = getSystemMetrics(1)
-
 dim = (width, height)
-
-f = cv2.VdideoWriterfourcc(*"XVID")
-
+f = cv2.VdideoWriter_fourcc(*"XVID")
 output = cv2.VideoWriter("test.mp4", f, 30.0, dim)
-
-nowstarttime = time.time()
+now_start_time = time.time()
 dur = 10
-endtime = nowstarttime + dur
+end_time = now_start_time + dur
 
 while True:
     image = pyautogui.screenshot()
+    frame_1 = np.array(image)
+    frame = cv2.cvtColor(frame_1, cv2.COLOR_BGR2RGB)
+    output.write(frame)
+    c_time = time.time()
+
+    if c_time > end_time:
+        break
+
+output.release()
+print("---END---")
+
